f6d5ce8273bf987453f3138f02627d717a35b0c4
AkWinchester <arsenijkiselev03@gmail.com>
2022-12-17 21:12:33 +0300

    1

---
+++ functions.py
@@ -0,0 +1,13 @@
+import h5py
+import io
+import pandas as pd
+import numpy as np
+
+def update_result(result, new_list):
+    result = np.array([*result, new_list])
+    return result
+
+def remove_duplicates(array):
+    u, idx = np.unique(array, axis=0, return_index=True)
+    array = u[idx.argsort()]
+    return array
--- test1.py
+++ test1.py
@@ -2,16 +2,16 @@ import h5py
 import io
 import pandas as pd
 import numpy as np
-
+from functions import remove_duplicates
 def update_result(result, new_list):
     result = np.array([*result, new_list])
     return result
 
 result = np.array([])
-with h5py.File('.\data_2022_10_16.hdf', 'r') as f:
+with h5py.File('data_2022_10_16.hdf', 'r') as f:
     print(list(f['GPS_1']['columns']))
-    utc_gps = np.array(f['GPS_1']['data'][:200:])
-    u, idx = np.unique(utc_gps, axis=0, return_index=True)
-    utc_gps = u[idx.argsort()]
+    utc_gps = np.array(f['GPS_1']['data'][::])
+    utc_gps = remove_duplicates(utc_gps)
+
     for i in range(0, len(utc_gps)):
-        print(i, ' ', utc_gps[i][0], ' ', utc_gps[i][1])
+        print(i, ' ', utc_gps[i][1])
